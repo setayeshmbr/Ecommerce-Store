8773307544cad9f0a3444eeb171cc96f1ea2e2d9
Setayesh Mohammadi <setayeshmbr26@gmail.com>
2021-06-12 13:07:35 +0430

    add UserAdmin

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
+
+from account.models import User
+UserAdmin.fieldsets[2][1]['fields'] = (
+    'is_staff',
+    'is_superuser',
+    'groups',
+    'user_permissions'
+)
+UserAdmin.list_display +=('is_superuser',)
+
+admin.site.register(User, UserAdmin)
